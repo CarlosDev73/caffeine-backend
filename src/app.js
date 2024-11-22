7c87964f94f9a66ab3d90119d8a7d928a3173664
Carlos Gutierrez <devcarlosg@gmail.com>
2024-11-22 19:09:35 -0400

    adding cors

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 import express from 'express';
 import morgan  from 'morgan';
+import cors from 'cors';
 import { v1Auth } from './v1/routes/index.js';
 
 const app = express(); 
 
-app.use(morgan('dev'));
 app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
 
+
+app.get('/api/v1/ping',(req,res)=>{
+  return res.status(200).json({
+    message: "pong",
+    error:[],
+    data:null
+})
+})
 
 app.use('/api/v1', v1Auth);
 
